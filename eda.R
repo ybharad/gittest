fc39f5987fdce0550d7d1bac729d141eb9a26b93
yash251198 <yash251198@gmail.com>
2020-04-26 16:57:26 -0700

    test 1 commit

--- eda.R
+++ eda.R
@@ -1,2 +1,10 @@
-setwd("/Users/ybhardw/yash/personal/Kaggle/springleaf/")
-data<- read.csv("train.csv",header=T)
+failrate<- function(df,col1,col2){
+  t <- as.data.frame.matrix(table(df[[col1]],df[[col2]]))
+  t$var <- rownames(t)
+  rownames(t) <- c()
+  colnames(t) <- c('no_fail','fail','var')
+  t <- t %>% mutate(fp = round((t$fail/(t$fail + t$no_fail))*100,2)) %>% 
+    arrange(desc(fp))
+  return(t)
+}
+
